4a33e9bcd3f749b972bd1ae536ee5f3a195d69c0
Germanmmrr1 <100393062@unimail.derby.ac.uk>
2025-07-24 17:24:43 +0200

    Update ecb_streamlitpy.py

--- ecb_streamlitpy.py
+++ ecb_streamlitpy.py
@@ -71,15 +71,14 @@ if st.session_state.get('start_game'):
 
     # Animation logic: advance year if animating
     if st.session_state['is_animating']:
-        current_year = st.session_state['auto_year']
-        if current_year < int(years[-1]):
-            next_year = current_year + 1
-            time.sleep(0.23)
-            st.session_state['auto_year'] = next_year
-            st.experimental_rerun()
-        else:
-            st.session_state['is_animating'] = False
-            # No st.experimental_rerun() here! End the animation cleanly.
+    current_year = st.session_state['auto_year']
+    if current_year < int(years[-1]):
+        next_year = current_year + 1
+        time.sleep(0.23)
+        st.session_state['auto_year'] = next_year
+        st.experimental_rerun()
+    else:
+        st.session_state['is_animating'] = False
 
 
     # The slider: value from session_state (also lets user drag)
